f39f915fab73c68c04449834fb3a4e51c763c06f
wadokh <115899643+wadokh@users.noreply.github.com>
2024-07-05 18:59:49 +0530

    add exercises 4.15 to 4.19

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -7,6 +7,8 @@ const api = supertest(app)
 
 const helper = require('./test_helper')
 
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
 const Blog = require('../models/blog')
 
 beforeEach(async () => {
@@ -91,6 +93,39 @@ describe('updating a specific note', () => {
     .expect('Content-Type', /application\/json/)
 
     assert.deepStrictEqual(resultBlog.body, blogToUpdate)
+  })
+})
+
+describe('when there is initially one user in db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('sekret', 10)
+    const user = new User({ username: 'root', passwordHash })
+
+    await user.save()
+  })
+
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'mluukkai',
+      name: 'Matti Luukkainen',
+      password: 'salainen',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map(u => u.username)
+    assert(usernames.includes(newUser.username))
   })
 })
 
