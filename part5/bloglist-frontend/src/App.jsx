261b36ce1218357307468c612163f6a068e3f811
wadokh <115899643+wadokh@users.noreply.github.com>
2024-07-07 16:16:44 +0530

    add exercise 5.2 to 5.4

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import Notification from './components/Notification'
 import loginService from './services/login'
 import blogService from './services/blogs'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [newTitle, setNewTitle] = useState('')
+  const [newAuthor, setNewAuthor] = useState('')
+  const [newUrl, setNewUrl] = useState('')
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [addMessage, setAddMessage] = useState(null)
   const [username, setUsername] = useState('') 
   const [password, setPassword] = useState('') 
   const [user, setUser] = useState(null)
@@ -16,6 +22,57 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+  useEffect(() => {
+    if (addMessage !== null) {
+      const timer = setTimeout(() => {
+        setAddMessage(null);
+      }, 5000);
+
+      // Cleanup the timer if the component unmounts or if addMessage changes
+      return () => clearTimeout(timer);
+    }
+  }, [addMessage]);
+
+  const addBlog = (event) => {
+    event.preventDefault()
+    const blogObject = {
+      title: newTitle,
+      author: newAuthor,
+      url: newUrl
+    }
+  
+    blogService
+      .create(blogObject)
+        .then(returnedBlog => {
+        setBlogs(blogs.concat(returnedBlog))
+        setNewTitle('')
+        setNewAuthor('')
+        setNewUrl('')
+        setAddMessage(`a new blog ${returnedBlog.title} by ${returnedBlog.author}`)
+      })
+  }
+
+  const handleTitleChange = (event) => {
+    setNewTitle(event.target.value)
+  }
+
+  const handleAuthorChange = (event) => {
+    setNewAuthor(event.target.value)
+  }
+
+  const handleUrlChange = (event) => {
+    setNewUrl(event.target.value)
+  }
+
   const handleLogin = async (event) => {
     event.preventDefault()
     
@@ -23,19 +80,30 @@ const App = () => {
       const user = await loginService.login({
         username, password,
       })
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      )
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
     } catch (exception) {
-      setErrorMessage('Wrong credentials')
+      setErrorMessage('Wrong username or password')
       setTimeout(() => {
         setErrorMessage(null)
       }, 5000)
     }
   }
 
+  const handleLogout = async (event) => {
+    event.preventDefault()
+    setUser(null)
+    window.localStorage.removeItem('loggedBlogappUser')
+  }
+
   const loginForm = () => (
     <form onSubmit={handleLogin}>
+      <Notification message={errorMessage} messageClass={"error"} />
       <div>
         username
           <input
@@ -70,7 +138,26 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} logged-in</p>
+      <Notification message={addMessage} messageClass={"added"} />
+      <p>{user.name} logged-in <button onClick={handleLogout}>logout</button></p>
+      <form onSubmit={addBlog}>
+        title: <input
+          value={newTitle}
+          onChange={handleTitleChange}
+        />
+        <br />
+        author: <input
+          value={newAuthor}
+          onChange={handleAuthorChange}
+        />
+        <br />
+        url: <input
+          value={newUrl}
+          onChange={handleUrlChange}
+        />
+        <br />
+        <button type="submit">create</button>
+      </form>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
